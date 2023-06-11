137f80e9c82800d8e6cedfb7125e775e8bda8d51
Your Name <you@1.com>
2023-06-11 22:30:16 +0530

    download file

--- PHASE3/weekend/spring-boot-file-upload-download/src/main/java/com/ecommerce/FileUploadDownloadController.java
+++ PHASE3/weekend/spring-boot-file-upload-download/src/main/java/com/ecommerce/FileUploadDownloadController.java
@@ -1,10 +1,19 @@
 package com.ecommerce;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -28,6 +37,27 @@ public class FileUploadDownloadController {
 		}
 
 		return result;
+	}
+	
+	@GetMapping("/download")
+	public ResponseEntity<Object> downloadFile() throws IOException {
+		String fileName = "static/dump.txt";
+		ClassLoader classLoader = new FileUploadDownloadController().getClass().getClassLoader();
+
+		File file = new File(classLoader.getResource(fileName).getFile());
+
+		InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+		HttpHeaders headers = new HttpHeaders();
+
+		headers.add("Content-Disposition", String.format("attachment;filename= \"%s\"", file.getName()));
+		headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		headers.add("Pragma", "no-cache");
+		headers.add("Expires", "0");
+
+		ResponseEntity<Object> responseEntity = ResponseEntity.ok().headers(headers).contentLength(file.length())
+				.contentType(MediaType.parseMediaType("application/txt")).body(resource);
+
+		return responseEntity;
 	}
 
 }
