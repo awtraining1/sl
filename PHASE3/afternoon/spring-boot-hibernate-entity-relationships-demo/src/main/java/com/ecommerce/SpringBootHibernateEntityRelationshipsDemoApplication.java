9f7186d91b3d1361d8a7ed6131060e4bdb32b527
Your Name <you@1.com>
2023-06-15 17:38:53 +0530

    update

--- PHASE3/afternoon/spring-boot-hibernate-entity-relationships-demo/src/main/java/com/ecommerce/SpringBootHibernateEntityRelationshipsDemoApplication.java
+++ PHASE3/afternoon/spring-boot-hibernate-entity-relationships-demo/src/main/java/com/ecommerce/SpringBootHibernateEntityRelationshipsDemoApplication.java
@@ -2,7 +2,11 @@ package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan({"com.ecommerce.entity","com.ecommerce","com.ecommerce.repos"})
+@EnableJpaRepositories
 @SpringBootApplication
 public class SpringBootHibernateEntityRelationshipsDemoApplication {
 
--- PHASE3/afternoon/spring-boot-hibernate-entity-relationships-demo/src/main/webapp/WEB-INF/views/index.jsp
+++ PHASE3/afternoon/spring-boot-hibernate-entity-relationships-demo/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,1 +1,8 @@
-Hello
+Create New Product
+<form action="add-product" method="POST">
+PROD NAME:<input name=pName> <br/>
+PRICE:<input name=pPrice> <br/>
+MANC NAME:<input name=mName> <br/>
+ADDR:<input name=mAddr> <br/>
+<input type=submit>
+</form>
