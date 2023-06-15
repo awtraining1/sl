a90e54a0e019751dd975bdea71decd8aafef8c8a
Your Name <you@1.com>
2023-06-15 15:51:05 +0530

    update

---
+++ PHASE3/afternoon/spring-boot-consuming-rest-webservice-demo/src/main/java/com/ecommerce/SpringBootConsumeRestWebserviceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsumeRestWebserviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConsumeRestWebserviceDemoApplication.class, args);
+	}
+
+}
