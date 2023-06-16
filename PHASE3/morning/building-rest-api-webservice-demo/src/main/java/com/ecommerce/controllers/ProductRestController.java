e800c445fbea9fcd1d21115c194405779578c80b
Your Name <you@1.com>
2023-06-16 12:34:26 +0530

    add product

--- PHASE3/morning/building-rest-api-webservice-demo/src/main/java/com/ecommerce/controllers/ProductRestController.java
+++ PHASE3/morning/building-rest-api-webservice-demo/src/main/java/com/ecommerce/controllers/ProductRestController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +22,23 @@ public class ProductRestController {
 	
 	// List all the products
 	@GetMapping(path="/list", produces = "application/json")
-	public ResponseEntity<List<EProduct>> displayData()
+	public ResponseEntity<List<EProduct>> listproducts()
 	{
 		List<EProduct> products = eProductRepo.findAll();		
 		
 		return new ResponseEntity<List<EProduct>>(products, HttpStatusCode.valueOf(200));		
 	}
 	
+	// Add a new product
+	@PostMapping(path="/add", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<EProduct> addProduct(@RequestBody EProduct eProduct){
+		eProduct = eProductRepo.save(eProduct);
+		
+		return new ResponseEntity<EProduct>(eProduct, HttpStatusCode.valueOf(200));		
+	}
+	
+	
+	// Show details of one product
 
 }
 
