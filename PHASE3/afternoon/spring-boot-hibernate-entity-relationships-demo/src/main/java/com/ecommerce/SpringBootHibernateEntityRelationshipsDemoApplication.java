f9b4ad82a7889547fbaa724a388ea4ac40f673d5
Your Name <you@1.com>
2023-06-16 14:49:00 +0530

    one to many example

--- PHASE3/afternoon/spring-boot-hibernate-entity-relationships-demo/src/main/java/com/ecommerce/SpringBootHibernateEntityRelationshipsDemoApplication.java
+++ PHASE3/afternoon/spring-boot-hibernate-entity-relationships-demo/src/main/java/com/ecommerce/SpringBootHibernateEntityRelationshipsDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan({"com.ecommerce.entity","com.ecommerce","com.ecommerce.repos"})
+@ComponentScan({"com.ecommerce.entity","com.ecommerce","com.ecommerce.repos", "com.ecommerce.relationship.o2m"})
 @EnableJpaRepositories
 @SpringBootApplication
 public class SpringBootHibernateEntityRelationshipsDemoApplication {
--- PHASE3/afternoon/spring-boot-hibernate-entity-relationships-demo/src/main/webapp/WEB-INF/views/index.jsp
+++ PHASE3/afternoon/spring-boot-hibernate-entity-relationships-demo/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,8 +1,1 @@
-Create New Product
-<form action="add-product" method="POST">
-PROD NAME:<input name=pName> <br/>
-PRICE:<input name=pPrice> <br/>
-MANC NAME:<input name=mName> <br/>
-ADDR:<input name=mAddr> <br/>
-<input type=submit>
-</form>
+Hello
