a4e11191e851927dd61eb65e17b288b4892577ec
Your Name <you@1.com>
2023-06-19 16:05:24 +0530

    Junit getting started

---
+++ PHASE3/afternoon/ecommerce.junit/src/main/java/com/ecommerce/junit/Test2.java
@@ -0,0 +1,24 @@
+package com.ecommerce.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test2 {
+
+	@Test
+	public void testSomething1() {
+		
+		//...
+		
+	}
+	
+	@Test
+	public void testSomething2() {
+		
+		//...
+		
+	}
+	
+
+}
---
+++ PHASE3/afternoon/ecommerce.junit/src/test/com/ecommerce/junit/Test1.java
@@ -0,0 +1,12 @@
+package com.ecommerce.junit;
+
+import org.junit.jupiter.api.Test;
+
+class Test1 {
+
+	@Test
+	void test() {
+		System.out.println("Not yet implemented");
+	}
+
+}
