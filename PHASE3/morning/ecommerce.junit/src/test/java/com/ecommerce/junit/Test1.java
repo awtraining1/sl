991f59be8ad5dbf5bd9c98a8d47f796e0b39ee15
Your Name <you@1.com>
2023-06-19 11:32:21 +0530

    getting started with a std test

--- PHASE3/morning/ecommerce.junit/src/test/java/com/ecommerce/junit/Test1.java
+++ PHASE3/morning/ecommerce.junit/src/test/java/com/ecommerce/junit/Test1.java
@@ -1,14 +1,54 @@
 package com.ecommerce.junit;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("JUnit 5 Standard Test Class Example")
 class Test1 {
+	
+	@BeforeAll()
+	public static void myBefAll() {
+		// create the JDBC Connection obj here
+		// that will used when the test cases are run
+		System.out.println("Inside myBefAll().");		
+	}
+	
+	@AfterAll
+	public static void myAfterAll() {
+		// close the JDBC Connection 
+		System.out.println("Inside myAfterAll().");		
+	}
 
+	@BeforeEach
+	public void myBefEach() {
+		// create the JDBC Statement obj here
+		// that will used when the test cases are run
+		System.out.println("Inside myBefEach().");		
+	}
+	
+	@AfterEach
+	public void myAfterEach() {
+		// close the JDBC Statement obj here
+		System.out.println("Inside myAfterEach().");
+		
+	}
+	
 	@Test
+	@DisplayName("Sample Test 1")
 	public void test1() {
-		System.out.println("Starting a test.");
+		System.out.println("Inside test1()");
+		// JDBC SQL Query to fetch all eproduct
+	}
+	
+	@Test
+	@DisplayName("Sample Test 2")
+	public void test2() {
+		System.out.println("Inside test2()");
+		// JDBC SQL Query to fetch all eproduct that have price>100
 	}
 
 }
