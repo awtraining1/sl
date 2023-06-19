8bc5985f82e44ef95d05032ebda2ef19f7cded36
Your Name <you@1.com>
2023-06-19 11:03:15 +0530

    first test

---
+++ PHASE3/morning/ecommerce.junit/src/test/java/com/ecommerce/junit/Test1.java
@@ -0,0 +1,14 @@
+package com.ecommerce.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test1 {
+
+	@Test
+	public void test1() {
+		System.out.println("Starting a test.");
+	}
+
+}
