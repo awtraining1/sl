d29f99d7542af5f665e6b6fcf0924af36bcf7390
Your Name <you@1.com>
2023-06-19 16:41:41 +0530

    standard test

--- PHASE3/afternoon/ecommerce.junit/src/test/java/com/ecommerce/junit/Test1.java
+++ PHASE3/afternoon/ecommerce.junit/src/test/java/com/ecommerce/junit/Test1.java
@@ -1,12 +1,52 @@
 package com.ecommerce.junit;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("This is my first Test class")
 class Test1 {
+	
+	@BeforeAll
+	static void myBeforeAll() {
+		// Create a Connectn Obj here that will used in the Test cases Test1,  Test2,....
+		System.out.println("Inside  myBeforeAll()");
+	}
+	
+	@AfterAll
+	static void myAfterAll() {
+		// Close the Connectn Obj here.
+		System.out.println("Inside  myAfterAll()");
+	}
 
+	@BeforeEach
+	void myBeforeEach() {
+		// Create a Statement Obj here that will used in the Test cases Test1,  Test2,....
+		System.out.println("Inside  myBeforeEach()");
+	}
+	
+	@AfterEach
+	void myAfterEach() {
+		// close Statement Obj here so that it will release system resources.
+		System.out.println("Inside  myAfterEach()");
+	}
+	
+	
 	@Test
-	void test() {
-		System.out.println("Not yet implemented");
+	@DisplayName("This is my first Test case Test 1")
+	void test1() {
+		// Test a JDBC SQL Query select * for eproduct
+		System.out.println("Test1 ");
+	}
+	
+	@Test
+	@DisplayName("This is my second Test case Test 2")
+	void test2() {
+		// Test a JDBC SQL Query for eproduct where price>1000
+		System.out.println("Test2 ");
 	}
 
 }
