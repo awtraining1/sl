11933c02a7b80812a962d10723a5aeb34cbad68d
Your Name <you@1.com>
2023-06-19 17:20:30 +0530

    conditional tests

---
+++ PHASE3/afternoon/ecommerce.junit/src/test/java/com/ecommerce/junit/ConditionalTest.java
@@ -0,0 +1,35 @@
+package com.ecommerce.junit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+class ConditionalTest {
+
+	@Test
+	@EnabledOnOs({OS.WINDOWS})
+	public void testAddOnWindows() {
+	Calculator cal =new Calculator();
+	
+	int x=2;
+	int y=5;
+	
+	assertEquals(7, cal.add(x, y));
+		
+	}
+	
+	@Test
+	@EnabledOnOs({OS.LINUX})
+	public void testAddOnLinux() {
+	Calculator cal =new Calculator();
+	
+	int x=2;
+	int y=5;
+	
+	assertEquals(7, cal.add(x, y));		
+	}
+
+
+}
