86f127a101bf7de45d16dfc2f2e94fd31967458d
Your Name <you@1.com>
2023-07-07 15:08:08 +0530

    update

---
+++ PHASE4/afternoon/TYPESCRIPT/build/hello.js
@@ -0,0 +1,7 @@
+"use strict";
+// Declaring primitives
+// In JS : let x1=10;
+// In TS
+let x1 = 10;
+// console.log("x1=" + (x1 + 25));
+console.log(`x1= ${x1 + 25}`);
---
+++ PHASE4/afternoon/TYPESCRIPT/src/hello.ts
@@ -0,0 +1,9 @@
+// Declaring primitives
+
+// In JS : let x1=10;
+
+// In TS
+let x1:number = 10;
+
+// console.log("x1=" + (x1 + 25));
+console.log(`x1= ${x1 + 25}`);
