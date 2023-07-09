eeb249561389b2834212d4dfa783399dc9f34c3f
Your Name <you@1.com>
2023-07-09 19:51:25 +0530

    update

--- PHASE4/afternoon/ANGULAR/my-first-ang-project/src/app/app.module.ts
+++ PHASE4/afternoon/ANGULAR/my-first-ang-project/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChildComponentComponent } from './child-component/child-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponentComponent
   ],
   imports: [
     BrowserModule
