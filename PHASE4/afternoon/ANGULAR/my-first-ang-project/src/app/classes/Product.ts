f4818f0e7f7b2d5dff906d7f1d6edc9706dce499
Your Name <you@1.com>
2023-07-10 15:38:35 +0530

    update

---
+++ PHASE4/afternoon/ANGULAR/my-first-ang-project/src/app/classes/Product.ts
@@ -0,0 +1,11 @@
+export class Product{
+
+  name:string='';
+  price:number=0;
+
+  constructor(name:string, price:number){
+    this.name = name;
+    this.price = price;
+  }
+
+}
--- PHASE4/afternoon/ANGULAR/my-first-ang-project/src/app/product/product.component.ts
+++ PHASE4/afternoon/ANGULAR/my-first-ang-project/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../classes/Product';
 
 @Component({
   selector: 'product',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-  name:string='HP Laptop';
-  price:number=2000;
+  // name:string='HP Laptop';
+  // price:number=2000;
+
+  product: Product = new Product("Laptop",2000);
+
 }
