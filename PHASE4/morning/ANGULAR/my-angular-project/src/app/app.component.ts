20dc820c12e9768a9444e37be3f58bf26f9ec281
Your Name <you@1.com>
2023-07-10 10:40:00 +0530

    update

--- PHASE4/morning/ANGULAR/my-angular-project/src/app/app.component.ts
+++ PHASE4/morning/ANGULAR/my-angular-project/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template:'<p> Blah Blah </p>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  title = 'MY FIRST ANGULAR PROJECT';
+
+  productPrice:string =20 + 'INR';
 }
--- PHASE4/morning/ANGULAR/my-angular-project/src/app/app.module.ts
+++ PHASE4/morning/ANGULAR/my-angular-project/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product-component/product-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
