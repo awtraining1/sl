59946eada902d1f338ceb4438f10f4f0bc34201f
Your Name <you@1.com>
2023-07-12 16:36:31 +0530

    update

--- PHASE4/afternoon/ANGULAR/my-first-ang-project/src/app/add-product/add-product.component.ts
+++ PHASE4/afternoon/ANGULAR/my-first-ang-project/src/app/add-product/add-product.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Product } from '../classes/Product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'add-product',
@@ -10,8 +11,14 @@ export class AddProductComponent {
 
   product:Product = new Product('',0,'',true,true);
 
+  constructor(private productService: ProductService) {
+  }
+
   OnSubmit():void{
-    console.log(`Form submitted with values ${this.product.name} , ${this.product.price} `)
+    console.log(`Form submitted with values ${this.product.name} , ${this.product.price} `);
+    this.productService.addProductToBackEnd(this.product.name,this.product.price).subscribe(
+      (response) => { this.product = response; },
+      (error) => { console.log(error); });
   }
 
 }
--- PHASE4/afternoon/ANGULAR/my-first-ang-project/src/app/product.service.ts
+++ PHASE4/afternoon/ANGULAR/my-first-ang-project/src/app/product.service.ts
@@ -8,32 +8,42 @@ import { Observable, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private backEndUrl ='http://localhost:8080/product';
+  private backEndUrl = 'http://localhost:8080/product';
 
   constructor(private http: HttpClient) { }
 
   //APIs
-  getAllProducts():Product[] {
+  getAllProducts(): Product[] {
 
     return [
-      new Product("Laptop", 60000,"assets/images/laptop.jpg", true, true),
+      new Product("Laptop", 60000, "assets/images/laptop.jpg", true, true),
       new Product("Keyboard", 560, "assets/images/keyboard.jpg", false, true),
-      new Product("Mouse", 600,'', false, false),
-      new Product("Monitor", 20000,'', true, false),
+      new Product("Mouse", 600, '', false, false),
+      new Product("Monitor", 20000, '', true, false),
     ];
   }
 
-  getAllProductsFromBackend():Observable<Product[]>  {
-    return this.http.get<Product[]>(`${this.backEndUrl}/list`);   
+  getAllProductsFromBackend(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.backEndUrl}/list`);
   }
 
-  getProduct(prodNum:number=0):Product {
+  getProduct(prodNum: number = 0): Product {
 
-    return  new Product("Laptop", 60000,"assets/images/laptop.jpg", true, true);
+    return new Product("Laptop", 60000, "assets/images/laptop.jpg", true, true);
   }
 
-  getProductFromBackend(id:string):Observable<Product>  {
-    return this.http.get<Product>(`${this.backEndUrl}/details/${id}`);   
+  getProductFromBackend(id: string): Observable<Product> {
+
+    return this.http.get<Product>(`${this.backEndUrl}/details/${id}`);
+  }
+
+  addProductToBackEnd(name: string, price: number): Observable<Product> {
+    return this.http.post<Product>(`${this.backEndUrl}/add`,
+      {
+        "name": name,
+        "price": price,
+      }
+    );
   }
 
 }
