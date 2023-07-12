3378c05bb9206e60cd2d0c63669d15f4b0fd776a
Your Name <you@1.com>
2023-07-12 09:47:34 +0530

    update

--- PHASE4/morning/ANGULAR/my-angular-project/src/app/add-product-using-reactive-form/add-product-using-reactive-form.component.ts
+++ PHASE4/morning/ANGULAR/my-angular-project/src/app/add-product-using-reactive-form/add-product-using-reactive-form.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-add-product-using-reactive-form',
+  selector: 'add-product-using-reactive-form',
   templateUrl: './add-product-using-reactive-form.component.html',
   styleUrls: ['./add-product-using-reactive-form.component.css']
 })
-export class AddProductUsingReactiveFormComponent {
+export class AddProductUsingReactiveFormComponent implements OnInit{
+
+  name:string = '';
+  price:number=0;
+
+  addProductForm!: FormGroup;
+
+  submitted:boolean=false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit():void{
+
+    this.addProductForm = this.formBuilder.group({
+      name: new FormControl('',Validators.required),
+      price:new FormControl<number>(0,[Validators.required, Validators.min(1), Validators.max(100000)])
+    });
+  }
+
+  get f() { return this.addProductForm.controls; }
+
+  onSubmit():void {
+    this.submitted = true;
+    
+    // stop here if form is invalid
+    if (this.addProductForm.invalid) {
+      return;
+    };
+
+    console.log(`Product details will be submitted to the backend`);
+  }
+
+
 
 }
