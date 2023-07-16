67c6dadd067b494936001cf0160fc5040e090e6b
Your Name <you@1.com>
2023-07-16 20:31:36 +0530

    ES6 class inheritance

--- PHASE4/weekend/JAVASCRIPT/ES6-classes.js
+++ PHASE4/weekend/JAVASCRIPT/ES6-classes.js
@@ -5,7 +5,29 @@ class Bus{
         this.modelYr=modelYr;
     }
 
-}
+    displayDetails = () => {
+        console.log(`BUS DETAILS Name=${this.name} and Model Year=${this.modelYr}`);
+    };
 
+    getName = () => this.name;
+    getModelYr = () => this.modelYr;
+
+};
+
+
+class SpaceBus extends Bus{
+
+    constructor(name, modelYr, destination){
+        super(name, modelYr);
+        this.destination=destination;
+    }
+
+    getDestination = () => this.destination;
+
+    displayDetails = () => {
+        //console.log(`SPACE BUS DETAILS Name=${this.name} and Model Year=${this.modelYr} Destination=${this.destination}`);
+        console.log(`SPACE BUS DETAILS ${this.getName()} ,  ${this.getModelYr()},  Destination=${this.destination}`);
+    };
+}
 
 
