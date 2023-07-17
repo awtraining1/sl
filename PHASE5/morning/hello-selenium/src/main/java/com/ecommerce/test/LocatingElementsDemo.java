f29882feb492b4455261d12a25b79ce816735312
Your Name <you@1.com>
2023-07-17 13:04:31 +0530

    locating elements

--- PHASE5/morning/hello-selenium/src/main/java/com/ecommerce/test/LocatingElementsDemo.java
+++ PHASE5/morning/hello-selenium/src/main/java/com/ecommerce/test/LocatingElementsDemo.java
@@ -18,15 +18,28 @@ public class LocatingElementsDemo {
 		
 		// Let's locate the first name text field by its id.
 		WebElement firstNameTF = driver.findElement(By.id("firstName"));				
-		firstNameTF.sendKeys("My First Name is ");
+		firstNameTF.sendKeys("Myname");
 		
 		// Let's locate the surname name text field by its name.
 		WebElement surNameTF = driver.findElement(By.name("lastName"));
-		surNameTF.sendKeys("My Surname is ...");
+		surNameTF.sendKeys("latName");
 		
 		// The Next button using its class
 		WebElement nextButton = driver.findElement(By.className("VfPpkd-vQzf8d"));
 		System.out.println("Text on the button is "+nextButton.getText());
+		nextButton.click();
+		
+//		String dayXPath ="/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div[1]/div/div[1]/input";
+//		WebElement dayTF = driver.findElement(By.xpath(dayXPath));
+//		dayTF.sendKeys("10");
+		
+//		String dayRelativeXPath = "//*[@id=\"day\"]";
+//		WebElement dayTF1 = driver.findElement(By.xpath(dayRelativeXPath));
+//		dayTF1.sendKeys("10");
+		
+		WebElement dayTF1 = driver.findElement(By.id("day"));
+		dayTF1.sendKeys("10");
+		
 		
 		//close the browser and quit.
 		//driver.close();
