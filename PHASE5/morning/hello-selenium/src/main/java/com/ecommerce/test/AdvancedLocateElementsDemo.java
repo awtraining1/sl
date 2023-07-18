a0a30eb6cd8703b2ec538e9c6bfb64b938b7c361
Your Name <you@1.com>
2023-07-18 10:32:39 +0530

    advanced locate elements

--- PHASE5/morning/hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocateElementsDemo.java
+++ PHASE5/morning/hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocateElementsDemo.java
@@ -1,6 +1,10 @@
 package com.ecommerce.test;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AdvancedLocateElementsDemo {
@@ -9,7 +13,13 @@ public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver(); // new FirefoxDriver();	
 		
+		String baseUrl = "File:///F:\\Users\\HomeWk\\git\\sl\\PHASE5\\morning\\hello-selenium\\src\\main\\resources\\test.html";
 		
+		driver.get(baseUrl);
+		
+		List<WebElement> inputAdminElements = driver.findElements(By.xpath("//input[contains(@id, 'admin')]"));
+		
+		System.out.println("inputAdminElements has "+inputAdminElements.size());
 		//close the browser and quit.
 		//driver.close();
 	}
