09f3ad2fe95cb447929075d0ba17eca3b53d318a
Your Name <you@1.com>
2023-07-18 11:26:05 +0530

    advanced locate elements

--- PHASE5/morning/hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocateElementsDemo.java
+++ PHASE5/morning/hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocateElementsDemo.java
@@ -7,28 +7,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AdvancedLocateElementsDemo {
 
-public static void main(String[] args) throws InterruptedException {
-		
-		WebDriver driver = new ChromeDriver(); // new FirefoxDriver();	
-		
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver(); // new FirefoxDriver();
+
+		// demoXPathCSSSelector(driver);
+
+		// close the browser and quit.
+		// driver.close();
+	}
+
+	static void demoElementAction(WebDriver driver) {
 		String baseUrl = "File:///F:\\Users\\HomeWk\\git\\sl\\PHASE5\\morning\\hello-selenium\\src\\main\\resources\\test.html";
-		
+
 		driver.get(baseUrl);
 		
+		
+	}
+
+	static void demoXPathCSSSelector(WebDriver driver) {
+		String baseUrl = "File:///F:\\Users\\HomeWk\\git\\sl\\PHASE5\\morning\\hello-selenium\\src\\main\\resources\\test.html";
+
+		driver.get(baseUrl);
+
 		List<WebElement> inputAdminElements = driver.findElements(By.xpath("//input[contains(@id, 'admin')]"));
-		
-		System.out.println("inputAdminElements has "+inputAdminElements.size());
-		
+
+		System.out.println("inputAdminElements has " + inputAdminElements.size());
+
 		// same as above but by using CSS selectors
-List<WebElement> inputAdminElementsUsingCSSSelector = driver.findElements(By.cssSelector("input[id*='admin']"));
-		
-		System.out.println("inputAdminElementsUsingCSSSelector has "+inputAdminElementsUsingCSSSelector.size());
-		
-		WebElement secondH3SiblingOfForm = driver.findElement(By.cssSelector("form ~ h3:nth-child(2)"));
-		System.out.println("secondH3SiblingOfForm text is " + secondH3SiblingOfForm.getText());
-		
-		//close the browser and quit.
-		//driver.close();
+		List<WebElement> inputAdminElementsUsingCSSSelector = driver.findElements(By.cssSelector("input[id*='admin']"));
+
+		System.out.println("inputAdminElementsUsingCSSSelector has " + inputAdminElementsUsingCSSSelector.size());
+
+		WebElement secondH3SiblingOfForm = driver.findElement(By.cssSelector("h3:nth-child(2)"));
+		System.out.println("secondH3SiblingOfForm text is " + secondH3SiblingOfForm);
 	}
 
 }
