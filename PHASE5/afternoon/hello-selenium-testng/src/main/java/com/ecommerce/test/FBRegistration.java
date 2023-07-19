bbaf52d5fbf0491a3e706b809db3ad4b2d2f7038
Your Name <you@1.com>
2023-07-19 17:58:54 +0530

    tesng getting started

---
+++ PHASE5/afternoon/hello-selenium-testng/src/main/java/com/ecommerce/test/FBRegistration.java
@@ -0,0 +1,83 @@
+package com.ecommerce.test;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class FBRegistration {
+	public WebDriver driver;
+	public String baseUrl = "file:///F:/Users/HomeWk/git/sl/PHASE5/morning/hello-selenium/src/main/resources/test.html";
+	
+	@Test(dataProvider = "dp")
+	public void searchGoogle() {
+		
+		driver.get(baseUrl);
+		
+		WebElement selectMonthElement = driver.findElement(By.id("month"));
+		Select selectMonth = new Select(selectMonthElement);
+
+		System.out.println("selectMonth is muti select " + selectMonth.isMultiple());
+
+		selectMonth.selectByIndex(0);
+		selectMonth.selectByIndex(6);
+
+		List<WebElement> optionsList = selectMonth.getAllSelectedOptions();
+
+		for (WebElement option : optionsList)
+			System.out.println("selectMonth selected options are " + option.getText());
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+	}
+
+	@DataProvider
+	public Object[][] dp() {
+		return new Object[][] { new Object[] { 1, "a" }, new Object[] { 2, "b" }, };
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+	}
+
+	@AfterTest
+	public void afterTest() {
+	}
+
+	@BeforeSuite
+	public void beforeSuite() {
+		driver = new ChromeDriver();		
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		driver.quit();
+	}
+
+}
