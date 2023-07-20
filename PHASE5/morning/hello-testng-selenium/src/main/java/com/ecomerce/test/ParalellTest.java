aac59cb03d5e2ec72e3be36a71c558f25390f92c
Your Name <you@1.com>
2023-07-20 10:55:33 +0530

    tesng dependency of tests

--- PHASE5/morning/hello-testng-selenium/src/main/java/com/ecomerce/test/ParalellTest.java
+++ PHASE5/morning/hello-testng-selenium/src/main/java/com/ecomerce/test/ParalellTest.java
@@ -15,77 +15,66 @@ import org.testng.annotations.Test;
 public class ParalellTest {
 	public String baseUrl = "https://www.facebook.com/r.php?locale=en_GB&display=page";
 	private WebDriver driver;
-	
-  @Test(groups="Account Creation")
-  static void facebookAccCreation(WebDriver driver) {
-	  System.out.println("Inside facebookAccCreation");
-	  
+
+	@Test(groups = {"Account Creation"})
+	public void facebookAccCreation() {
+		System.out.println("Inside facebookAccCreation");
+
 		String baseUrl = "https://www.facebook.com/r.php?locale=en_GB&display=page";
 
 		driver.get(baseUrl);
-		
+
 		String cssDay = "#day";
-		WebElement cssDaySelect = driver.findElement(By.cssSelector(cssDay));	
-		
-		
-		Select daySelect =  new Select(cssDaySelect);
+		WebElement cssDaySelect = driver.findElement(By.cssSelector(cssDay));
+
+		Select daySelect = new Select(cssDaySelect);
 		daySelect.selectByVisibleText("11");
-	
-		WebElement cssGenderRadio = driver.findElement(By.cssSelector("span > span > input[type='radio'][value='2']" ));
+
+		WebElement cssGenderRadio = driver.findElement(By.cssSelector("span > span > input[type='radio'][value='2']"));
 		cssGenderRadio.click();
 		
-		// Demo find by tag name
-		List<WebElement> titleTagElement = driver.findElements(By.tagName("title"));
-		
-		JavascriptExecutor js = (JavascriptExecutor) driver; 
-		js.executeScript("console.log(arguments[0].innerText)", titleTagElement);
-		
-		
-		System.out.println("WebPage Title is "+titleTagElement.get(0).getText());
-		
+		System.out.println("Gender is enabled = " + cssGenderRadio.isSelected());
+
 	}
 
-  
-  @Test(groups="Account Creation", dependsOnGroups = {"Launch"})
-  static void googleAccCreation(WebDriver driver) {
-	  	System.out.println("Inside googleAccCreation");
-	  
+	@Test(groups = "Account Creation", dependsOnGroups = { "Launch" })
+	public void googleAccCreation() {
+		System.out.println("Inside googleAccCreation");
+
 		String baseUrl = "https://accounts.google.com/signup/v2?biz=true&flowEntry=SignUp";
 		driver.get(baseUrl);
-		
+
 		// Let's locate the first name text field by its id.
-				WebElement firstNameTF = driver.findElement(By.id("firstName"));				
-				firstNameTF.sendKeys("Myname");
-				
-				// Let's locate the surname name text field by its name.
-				WebElement surNameTF = driver.findElement(By.name("lastName"));
-				surNameTF.sendKeys("latName");
-				
-				// The Next button using its class
-				WebElement nextButton = driver.findElement(By.className("VfPpkd-vQzf8d"));
-				System.out.println("Text on the button is "+nextButton.getText());					
+		WebElement firstNameTF = driver.findElement(By.id("firstName"));
+		firstNameTF.sendKeys("Myname");
+
+		// Let's locate the surname name text field by its name.
+		WebElement surNameTF = driver.findElement(By.name("lastName"));
+		surNameTF.sendKeys("latName");
+
+		// The Next button using its class
+		WebElement nextButton = driver.findElement(By.className("VfPpkd-vQzf8d"));
+		System.out.println("Text on the button is " + nextButton.getText());
 
 	}
-  
-  @Test(groups="Launch")
-  static void googleWebsiteTitle (WebDriver driver) {
-	  System.out.println("Inside googleWebsiteTitle");
-	  
-	  String baseUrl = "https://www.google.com";
-		driver.get(baseUrl);
-		
-		System.out.println("Google Title is "+driver.getTitle());
-  }
 
-  
-  @BeforeClass
+	@Test(groups = "Launch")
+	public void googleWebsiteTitle() {
+		System.out.println("Inside googleWebsiteTitle");
+
+		String baseUrl = "https://www.google.com";
+		driver.get(baseUrl);
+
+		System.out.println("Google Title is " + driver.getTitle());
+	}
+
+	@BeforeClass
 	public void beforeClass() {
 		driver = new ChromeDriver();
-      driver.get(baseUrl);
 	}
 
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
-	} 
+	}
 }
