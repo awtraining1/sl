540d29bf1a618ae0ab56505473cc01b5303698b1
Your Name <you@1.com>
2023-07-20 14:25:03 +0530

    tesng configuration annotation

--- PHASE5/afternoon/hello-selenium-testng/src/main/java/com/ecommerce/test/FBRegistration.java
+++ PHASE5/afternoon/hello-selenium-testng/src/main/java/com/ecommerce/test/FBRegistration.java
@@ -24,7 +24,7 @@ public class FBRegistration {
 	
 	@Test()
 	public void searchGoogle() {
-		System.out.printf("Inside %s and thread-id is %s","searchGoogle", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","searchGoogle", Thread.currentThread().getId()  );
 		
 		driver.get("http://www.google.com");
 		
@@ -33,7 +33,7 @@ public class FBRegistration {
 	
 	@Test()
 	public void fbAccount() {
-		System.out.printf("Inside %s and thread-id is %s","fbAccount", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","fbAccount", Thread.currentThread().getId()  );
 		
 		String baseUrl = "https://www.facebook.com/r.php?locale=en_GB&display=page";
 
@@ -53,7 +53,7 @@ public class FBRegistration {
 	
 	@Test()
 	public void googleAccount() {
-		System.out.printf("Inside %s and thread-id is %s","googleAccount", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","googleAccount", Thread.currentThread().getId()  );
 				
 		String baseUrl = "https://accounts.google.com/signup/v2?biz=true&flowEntry=SignUp";
 		driver.get(baseUrl);
@@ -74,16 +74,16 @@ public class FBRegistration {
 	
 	@Test()
 	public void f1() {
-		System.out.printf("Inside %s and thread-id is %s","f1", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","f1", Thread.currentThread().getId()  );
 	}
 	@Test()
 	public void f2() {
-		System.out.printf("Inside %s and thread-id is %s","f2", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","f2", Thread.currentThread().getId()  );
 	}
 	
 	@Test()
 	public void testLocalPage() {
-		System.out.printf("Inside %s and thread-id is %s","testLocalPage", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","testLocalPage", Thread.currentThread().getId()  );
 		
 		driver.get(baseUrl);
 		
@@ -103,12 +103,12 @@ public class FBRegistration {
 
 	@BeforeMethod
 	public void beforeMethod() {
-		System.out.printf("Inside %s and thread-id is %s","beforeMethod", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","beforeMethod", Thread.currentThread().getId()  );
 	}
 
 	@AfterMethod
 	public void afterMethod() {
-		System.out.printf("Inside %s and thread-id is %s","afterMethod", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","afterMethod", Thread.currentThread().getId()  );
 	}
 
 	@DataProvider
@@ -118,27 +118,27 @@ public class FBRegistration {
 
 	@BeforeClass
 	public void beforeClass() {
-		System.out.printf("Inside %s and thread-id is %s","beforeClass", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","beforeClass", Thread.currentThread().getId()  );
 	}
 
 	@AfterClass
 	public void afterClass() {
-		System.out.printf("Inside %s and thread-id is %s","afterClass", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n ","afterClass", Thread.currentThread().getId()  );
 	}
 
 	@BeforeTest
 	public void beforeTest() {
-		System.out.printf("Inside %s and thread-id is %s","beforeTest", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","beforeTest", Thread.currentThread().getId()  );
 	}
 
 	@AfterTest
 	public void afterTest() {
-		System.out.printf("Inside %s and thread-id is %s","afterTest", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","afterTest", Thread.currentThread().getId()  );
 	}
 
 	@BeforeSuite
 	public void beforeSuite() {
-		System.out.printf("Inside %s and thread-id is %s","beforeSuite", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","beforeSuite", Thread.currentThread().getId()  );
 		//System.setProperty("webdriver.chrome.driver","C:\\Users\\HomeWk\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
 		driver = new ChromeDriver();
@@ -146,7 +146,7 @@ public class FBRegistration {
 
 	@AfterSuite
 	public void afterSuite() {
-		System.out.printf("Inside %s and thread-id is %s","afterSuite", Thread.currentThread().getId()  );
+		System.out.printf("Inside %s and thread-id is %s \n","afterSuite", Thread.currentThread().getId()  );
 		driver.quit();
 	}
 
