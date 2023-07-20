94e8a189a056b575aacb6b1ca9e5d40b2252d364
Your Name <you@1.com>
2023-07-20 11:46:22 +0530

    tesng assertions

--- PHASE5/morning/hello-testng-selenium/src/main/java/com/ecomerce/test/ParalellTest.java
+++ PHASE5/morning/hello-testng-selenium/src/main/java/com/ecomerce/test/ParalellTest.java
@@ -9,10 +9,13 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class ParalellTest {
 	public String baseUrl = "https://www.facebook.com/r.php?locale=en_GB&display=page";
 	private WebDriver driver;
+	
+	SoftAssert softassert = new SoftAssert();
 
 	@Test(groups = {"Account Creation"})
 	public void facebookAccCreation() {
@@ -25,6 +28,8 @@ public class ParalellTest {
 
 		String cssDay = "#day";
 		WebElement cssDaySelect = driver.findElement(By.cssSelector(cssDay));
+		
+		softassert.assertNotNull(cssDaySelect);
 
 		Select daySelect = new Select(cssDaySelect);
 		daySelect.selectByVisibleText("11");
@@ -32,8 +37,11 @@ public class ParalellTest {
 		WebElement cssGenderRadio = driver.findElement(By.cssSelector("span > span > input[type='radio'][value='2']"));
 		cssGenderRadio.click();
 		
+		softassert.assertTrue(cssGenderRadio.isSelected());
+		
 		System.out.println("Gender is enabled = " + cssGenderRadio.isSelected());
-
+		
+		softassert.assertAll(" Day or/and Gender assertion failed");		
 	}
 
 	@Test(groups = "Account Creation", dependsOnGroups = { "Launch" }, 
@@ -56,6 +64,10 @@ public class ParalellTest {
 		// The Next button using its class
 		WebElement nextButton = driver.findElement(By.className("VfPpkd-vQzf8d"));
 		System.out.println("Text on the button is " + nextButton.getText());
+		
+		softassert.assertEquals("Next", nextButton.getText());
+		
+		softassert.assertAll(" Google Next button text did not match");
 
 	}
 
@@ -69,6 +81,10 @@ public class ParalellTest {
 		driver.get(baseUrl);
 
 		System.out.println("Google Title is " + driver.getTitle());
+		
+		softassert.assertEquals("Google", driver.getTitle());
+		
+		softassert.assertAll(" Google title did not match");
 	}
 	
 	@Test
