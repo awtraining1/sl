418ffc6be4dc524c618a44810403ef2274680a65
Your Name <you@1.com>
2023-07-29 20:12:20 +0530

    angular components

--- PHASE4/weekend/ANGULAR/my-app/src/app/app.component.ts
+++ PHASE4/weekend/ANGULAR/my-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+@Component(
+  {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+  }
+)
 export class AppComponent {
-  title = 'my-app';
+  title = 'my-app 123';
+  myName = 'Jhonny Walker';
 }
+
