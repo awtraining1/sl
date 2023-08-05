ff2bf1c6f57b8ccc53bf88729c7bb8da9b7fc93d
Your Name <you@1.com>
2023-08-05 22:10:37 +0530

    spring boot integration

--- PHASE4/weekend/ANGULAR/my-app/src/app/product.service.ts
+++ PHASE4/weekend/ANGULAR/my-app/src/app/product.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product';
 
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
+  backEndUrl :String = "http://localhost:8080/api/product/v1";
+
+  constructor(private httpClient: HttpClient) { };
 
   products:Product[]=[
     new Product("Laptop", 60000, true, true),
@@ -13,15 +21,18 @@ export class ProductService {
     new Product("Monitor", 20000, true, false),
   ];
 
+  //SpringBoot integration
+  getAllProductsFromSpringBootBackend(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(`${this.backEndUrl}/list`);
+  }
+
+  //Sample/Dummy data providers
   getAllProducts(): Product[] {
 
     return this.products;
   }
 
   getProducts(index:number): Product {
-    return this.products[index];
-  }
+    return this.products[index]; }
 
-
-  constructor() { }
 }
